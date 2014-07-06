c91eb68826d56611ab7196b0858548844b0e1806
Matthias Beyer <mail@beyermatthias.de>
2014-07-06 16:20:00 +0200

    Added setup command

---
+++ src/setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/sh
+
+# setup the working environment.
+
+VERBOSE=0
+
+while getopts "v" OPTIONS
+do
+    case $OPTIONS
+        v)
+            VERBOSE=1
+            ;;
+    esac
+done
+
+verbose() {
+    [[ $VERBOSE -eq 1 ]]
+}
+
+output() {
+    verbose && echo "[$0]: $1"
+}
+
+setup_apply_mbox() {
+    mkdir /tmp/kernel_apply
+    mkdir /tmp/kernel_apply/cur
+    mkdir /tmp/kernel_apply/new
+    mkdir /tmp/kernel_apply/tmp
+}
+
+setup_apply_mbox
